daa9eaa428a8cea9cee988b0ca6cd29a26210161
Damian0401 <szkola_abc@o2.pl>
2023-07-09 14:05:34 +0200

    Updated project to .NET 7, added MediatR, FluentValidation and EF Core

---
+++ backend/CityShare.Backend.Persistence/CityShareDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CityShare.Backend.Persistence;
+
+public class CityShareDbContext : DbContext
+{
+    public CityShareDbContext(DbContextOptions<CityShareDbContext> options)
+        : base(options)
+    {
+        
+    }
+}
---
+++ backend/CityShare.Backend.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using CityShare.Backend.Domain.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CityShare.Backend.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString(ConnectionStrings.CityShareDB);
+
+        services.AddDbContext<CityShareDbContext>(options => options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
