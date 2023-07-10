55abfbd593662c2fb9b371e3d639922984636495
Damian Gnieciak <77583760+Damian0401@users.noreply.github.com>
2023-07-10 20:27:58 +0200

    Backend validation flow (#3)
    * Added Result class and ValidationPipelineBehaviour
    
    * Corrected type

---
+++ backend/CityShare.Backend.Domain/Shared/Error.cs
@@ -0,0 +1,75 @@
+﻿using CityShare.Backend.Domain.Enums;
+
+namespace CityShare.Backend.Domain.Shared;
+
+public class Error : IEquatable<Error>
+{
+    public static Error None => new Error(ErrorTypes.None, string.Empty);
+
+    public Error(ErrorTypes type, string message)
+    {
+        Type = type;
+        Messages = new() { message };
+    }    
+    
+    public Error(ErrorTypes type, List<string> messages)
+    {
+        Type = type;
+        Messages = messages;
+    }
+
+    public ErrorTypes Type { get; }
+    public List<string> Messages { get; }
+
+    public static bool operator ==(Error? a, Error? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Type == b.Type && a.Messages.SequenceEqual(b.Messages);
+    }
+
+    public static bool operator !=(Error? a, Error? b) => !(a == b);
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(obj, null))
+        {
+            return false;
+        }
+
+        return obj is Error error && Equals(error);
+    }
+
+    public override int GetHashCode()
+    {
+        HashCode hashCode = default;
+
+        hashCode.Add(Type);
+        hashCode.Add(Messages);
+
+        return hashCode.ToHashCode();
+    }
+
+    public bool Equals(Error? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Type == other.Type && Messages.SequenceEqual(other.Messages);
+    }
+}
---
+++ backend/CityShare.Backend.Domain/Shared/ResultT.cs
@@ -0,0 +1,18 @@
+﻿namespace CityShare.Backend.Domain.Shared;
+
+public class Result<TValue> : Result
+{
+    public Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        Value = value;
+    }
+    
+    public TValue? Value { get; }
+
+    public static implicit operator Result<TValue>(TValue? value) 
+        => new Result<TValue>(value, true, Error.None);
+
+    public static Result<TValue> Success(TValue value) => new(value, true, Error.None);
+    public static new Result<TValue> Failure(Error error) => new(default, false, error);
+}
