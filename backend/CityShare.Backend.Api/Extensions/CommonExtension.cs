123996b24361fff9931e813dc217829f139f040e
Damian0401 <szkola_abc@o2.pl>
2023-07-26 20:24:39 +0200

    Added jwt bearer security schema to swagger

--- backend/CityShare.Backend.Api/Extensions/CommonExtension.cs
+++ backend/CityShare.Backend.Api/Extensions/CommonExtension.cs
@@ -1,5 +1,6 @@
 ﻿using CityShare.Backend.Domain.Constants;
 using FluentValidation;
+using Microsoft.OpenApi.Models;
 using System.Globalization;
 
 namespace CityShare.Backend.Api.Extensions;
@@ -17,7 +18,33 @@ public static class CommonExtension
 
         services.AddEndpointsApiExplorer();
 
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "CityShare API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+            });
+        });
 
         return services;
     }
