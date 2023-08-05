9c4dbf37177770057092e059a0e5c52026d1d07e
Damian0401 <szkola_abc@o2.pl>
2023-08-05 19:23:51 +0200

    Added missing cancellation tokens, moved ResultResolver to Common namespace

--- backend/CityShare.Backend.Api/Api/V1/Map.cs
+++ backend/CityShare.Backend.Api/Api/V1/Map.cs
@@ -1,21 +1,30 @@
-﻿using CityShare.Backend.Application.Map.Queries.Reverse;
+﻿using CityShare.Backend.Api.Common;
+using CityShare.Backend.Application.Map.Queries.Reverse;
 using CityShare.Backend.Application.Map.Queries.Search;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityShare.Backend.Api.Api.V1;
 
 public class Map
 {
-    public static async Task<IResult> Search(string query, IMediator mediator)
+    public static async Task<IResult> Search(
+        [FromQuery] string query,
+        IMediator mediator,
+        CancellationToken cancellationToken)
     {
-        var result = await mediator.Send(new SearchQuery(query));
+        var result = await mediator.Send(new SearchQuery(query), cancellationToken);
 
         return ResultResolver.Resolve(result);
     }
 
-    public static async  Task<IResult> Reverse(double x, double y, IMediator mediator)
+    public static async Task<IResult> Reverse(
+        [FromQuery] double x,
+        [FromQuery] double y, 
+        IMediator mediator,
+        CancellationToken cancellationToken)
     {
-        var result = await mediator.Send(new ReverseQuery(x, y));
+        var result = await mediator.Send(new ReverseQuery(x, y), cancellationToken);
 
         return ResultResolver.Resolve(result);
     }
