19d3ce5c3fabd7fa77ba9acb7edd792ceb0afd0e
Damian0401 <szkola_abc@o2.pl>
2023-08-21 22:05:06 +0200

    Added OptionSelect and MultiOptionSelect

---
+++ frontend/src/components/multi-option-select/IMultiOptionSelect.ts
@@ -0,0 +1,11 @@
+import { IOption } from "../../common/interfaces";
+
+export interface IMultiOptionSelectProps {
+  errors?: string | string[];
+  touched?: boolean;
+  name: string;
+  label: string;
+  options: IOption[];
+  isRequired?: boolean;
+  onChange: (value: number[]) => void;
+}
