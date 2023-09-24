6080e2c5ce15560a93a2009b52dafcc8598a45a2
Damian0401 <szkola_abc@o2.pl>
2023-09-24 19:18:04 +0200

    Added architecture tests

---
+++ backend/CityShare.Backend.Tests/ArchitectureTests/Base/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace CityShare.Backend.Tests.ArchitectureTests.Base;
+
+public class TestBase
+{
+    protected Assembly ApplicationAssembly { get; }
+    protected Assembly DomainAssembly { get; }
+    protected Assembly InfrastructureAssembly { get; }
+    protected Assembly PersistenceAssembly { get; }    
+    protected string ApplicationNamespace { get; }
+    protected string DomainNamespace { get; }
+    protected string InfrastructureNamespace { get; }
+    protected string PersistenceNamespace { get; }
+
+    public TestBase()
+    {
+        ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
+        DomainAssembly = typeof(Domain.Shared.Result).Assembly;
+        InfrastructureAssembly = typeof(Infrastructure.DependencyInjection).Assembly;
+        PersistenceAssembly = typeof(Persistence.DependencyInjection).Assembly;
+
+        ApplicationNamespace = "CityShare.Backend.Application";
+        DomainNamespace = "CityShare.Backend.Domain";
+        InfrastructureNamespace = "CityShare.Backend.Infrastructure";
+        PersistenceNamespace = "CityShare.Backend.Persistance";
+    }
+}
---
+++ backend/CityShare.Backend.Tests/ArchitectureTests/InfrastructureTests.cs
@@ -0,0 +1,41 @@
+﻿using CityShare.Backend.Domain.Constants;
+using CityShare.Backend.Tests.ArchitectureTests.Base;
+using NetArchTest.Rules;
+
+namespace CityShare.Backend.Tests.ArchitectureTests;
+
+public class InfrastructureTests : TestBase
+{
+    [Fact]
+    public void Infrastructure_ShouldNot_DependOnPersistence()
+    {
+        // Arrange
+
+        // Act
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(PersistenceNamespace)
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+    }
+
+
+    [Fact]
+    public void Infrastructure_ShouldNot_ContaintServiceInterfaces()
+    {
+        // Arrange
+
+        // Act
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .That()
+            .HaveNameEndingWith(ClassNames.Suffix.Service)
+            .ShouldNot()
+            .BeInterfaces()
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+    }
+}
